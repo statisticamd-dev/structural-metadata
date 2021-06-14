f7778e967c4224bc6d2afdb20c80d3527dec4b1c
Veaceslav Chirilov <slavicchirilov@yahoo.com>
2021-06-14 19:00:19 +0300

    Integrated swagger into structural metadata api with a sample controller.

--- Parstat.StructuralMetadata/Presentation/Presentation.WebApi/Startup.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,21 @@ namespace Presentation.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Register swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Structural Metadata",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Parstat",
+                        Email = string.Empty,
+                        Url = new Uri("https://parstat.md")
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +51,16 @@ namespace Presentation.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Structural Metadata API V1.0");
+                c.RoutePrefix = string.Empty;
+                
+            });
 
             app.UseHttpsRedirection();
 
