2965b161b0b7a0b4e944cb6318eebbf9fd82167f
Veaceslav Chirilov <slavicchirilov@yahoo.com>
2021-06-30 10:53:01 +0300

    Fixed type for VersionDate field in abstract class Domain.

--- Parstat.StructuralMetadata/Presentation/Presentation.Domain/StructMeta/Domain.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Domain/StructMeta/Domain.cs
@@ -11,7 +11,7 @@ namespace Presentation.Domain.StructMeta
         public string Name { get; set; }
         public string Description { get; set; }
         public string Version { get; set; }
-        public string VersionDate { get; set; }
+        public DateTime VersionDate { get; set; }
         public string VersionRationale { get; set; }        
     }
 }
