9ba7718459b298c03f8be030e60193c682759522
florianika <florianika@gmail.com>
2021-06-30 09:23:16 +0200

    fix DateTime property

--- Parstat.StructuralMetadata/Presentation/Presentation.Domain/StructMeta/abstracts/gsim/AbstractIdentifiableArtefact.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Domain/StructMeta/abstracts/gsim/AbstractIdentifiableArtefact.cs
@@ -1,14 +1,15 @@
+using System;
 using Presentation.Domain.StructMeta.interfaces.gsim;
+
 namespace Presentation.Domain.StructMeta.abstracts.gsim
 {
-    public class AbstractIdentifiableArtefact : AbstractDomain, IIdentifiableArtefact
+    public abstract class AbstractIdentifiableArtefact : AbstractDomain, IIdentifiableArtefact
     {
         public string LocalId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Version { get; set; }
-        //TODO add property Date: VersionDate here
-        //public Date VersionDate { get; set; }
+        public DateTime VersionDate { get; set; }
         public string VersionRationale { get; set; }
     }
 }
