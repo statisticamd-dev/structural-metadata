f2c9d74b14557d3ef360a97427ff92d783ea803c
Veaceslav Chirilov <slavicchirilov@yahoo.com>
2021-07-01 19:24:12 +0300

    Added 3 items. IConcept, IUnitType interfaces and class UnitType

---
+++ Parstat.StructuralMetadata/Presentation/Presentation.Common.Domain/StructuralMetadata/Interfaces/Gsim/IConcept.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation.Common.Domain.StructuralMetadata.Interfaces.Gsim
+{
+    public interface IConcept: IIdentifiableArtefact
+    {
+        string Definition { get; set; }
+    }
+}
---
+++ Parstat.StructuralMetadata/Presentation/Presentation.Common.Domain/StructuralMetadata/Interfaces/Gsim/IUnitType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentation.Common.Domain.StructuralMetadata.Interfaces.Gsim
+{
+    public interface IUnitType: IConcept
+    {
+
+    }
+}
