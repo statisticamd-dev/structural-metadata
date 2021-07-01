a9d1774f00a26d5ee862fd7f54bc918921687b55
Veaceslav Chirilov <slavicchirilov@yahoo.com>
2021-07-01 19:25:09 +0300

    Added class UnitType

--- Parstat.StructuralMetadata/Presentation/Presentation.Domain/UnitType.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Domain/UnitType.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Presentation.Common.Domain.StructuralMetadata.Abstracts.Gsim;
+using Presentation.Common.Domain.StructuralMetadata.Interfaces.Gsim;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Presentation.Domain
 {
-    class UnitType
+    public class UnitType : AbstractIdentifiableArtefact, IUnitType
     {
+        public string Definition { get; set; }
     }
 }
