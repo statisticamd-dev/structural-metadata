351ad4dc739e8409687b3e845c24bc2622a2dd32
florianika <florianika@gmail.com>
2021-08-12 13:28:07 +0200

    fixing multilanuage

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/Common/Behaviours/RequestLogger.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/Common/Behaviours/RequestLogger.cs
@@ -8,7 +8,7 @@ namespace Presentation.Application.Common.Behaviours
 {
     public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
     {
-         private readonly ILogger _logger;
+        private readonly ILogger _logger;
         private readonly ICurrentUserService _currentUserService;
 
         public RequestLogger(ILogger<TRequest> logger, ICurrentUserService currentUserService)
