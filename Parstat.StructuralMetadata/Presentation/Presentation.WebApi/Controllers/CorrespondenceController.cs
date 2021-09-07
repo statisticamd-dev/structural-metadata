c00e16c92d3cb19b759cb126317483c5d83405be
florianika <florianika@gmail.com>
2021-09-07 11:31:55 +0200

    created correspondence controller

---
+++ Parstat.StructuralMetadata/Presentation/Presentation.WebApi/Controllers/CorrespondenceController.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Application.Correspondences.Queries.GetCorrespondence;
+using Presentation.Application.Correspondences.Queries.GetCorrespondences;
+
+namespace Presentation.WebApi.Controllers
+{
+    public class CorrespondenceController : BaseController
+    {
+        [HttpGet("/nodeset/{nodesetId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<CorrespondencesVm>> GetAllByNodeset(long nodesetId, string language) => Ok(await Mediator.Send(new GetNodesetCorrespondencesQuery {NodeSetId = nodesetId, Language = language}));
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<CorrespondenceVm>> Get(long id, string language) => Ok(await Mediator.Send(new GetCorrespondenceQuery {Id = id, Language = language}));
+    }
+}
