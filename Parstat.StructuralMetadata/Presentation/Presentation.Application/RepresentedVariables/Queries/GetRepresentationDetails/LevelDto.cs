a338e8b09c40b3d6b70c5ede04f4b19eae4ccbc2
florianika <florianika@gmail.com>
2021-11-15 14:36:06 +0100

    debugging21

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/RepresentedVariables/Queries/GetRepresentationDetails/LevelDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/RepresentedVariables/Queries/GetRepresentationDetails/LevelDto.cs
@@ -12,7 +12,7 @@ namespace Presentation.Application.RepresentedVariables.Queries.GetRepresentatio
         public string Name { get; set; }
         public string Description { get; set; }
         public int LevelNumber { get; set; }
-        public List<NodeDto> Nodes { get; set; }
+        //public List<NodeDto> Nodes { get; set; }
 
          public void Mapping(Profile profile)
         {
@@ -22,8 +22,8 @@ namespace Presentation.Application.RepresentedVariables.Queries.GetRepresentatio
             profile.CreateMap<Level, LevelDto>()
                 .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : null))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : null))
-                .ForMember(d => d.Nodes, opt => opt.MapFrom(s => s.Nodes));
+                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : null));
+                //.ForMember(d => d.Nodes, opt => opt.MapFrom(s => s.Nodes));
         } 
     }
 }
