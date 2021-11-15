db5ee2ed175a87fe056198c9a8a71b079ca39b4e
florianika <florianika@gmail.com>
2021-11-15 13:42:53 +0100

    debugging15

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/RepresentedVariables/Queries/GetRepresentationDetails/NodeSetDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/RepresentedVariables/Queries/GetRepresentationDetails/NodeSetDto.cs
@@ -21,13 +21,11 @@ namespace Presentation.Application.RepresentedVariables.Queries.GetRepresentatio
             //default english
             string language = "en";
             profile.CreateMap<NodeSet, NodeSetDto>()
-                .ForMember(d => d.LocalId, opt => opt.MapFrom(s => s.LocalId))
-                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : null))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : null))
-                .ForMember(d => d.Version, opt => opt.MapFrom(s => s.Version))
-                .ForMember(d => d.Definition, opt => opt.MapFrom(s => s.Definition != null ? s.Definition.Text(language) : null))
-                .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : null))
-                .ForMember(d => d.Link, opt => opt.MapFrom(s => s.Link != null ? s.Link.Text(language) : null));
+               .ForMember(ns => ns.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : null))
+                .ForMember(ns => ns.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : null))
+                .ForMember(ns => ns.Definition, opt => opt.MapFrom(s => s.Definition != null ? s.Definition.Text(language) : null))
+                .ForMember(ns => ns.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale.Text(language)))
+                .ForMember(ns => ns.Link, opt => opt.MapFrom(s => s.Link != null ? s.Link.Text(language) : null));
                 //.ForMember(d => d.NodeSetType, opt => opt.MapFrom(s => s.NodeSetType));
                 //.ForMember(d => d.Nodes, opt => opt.MapFrom(s => s.Nodes));
         } 
