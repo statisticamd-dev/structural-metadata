3d58fb8b11b85dac23b246a45375081ba178452f
florianika <florianika@gmail.com>
2021-11-30 11:49:20 +0100

    fixing statistical classification api

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/NoteSets/StatisticalClassifications/Queries/GetStatisticalClassifications/StatisticalClassificationDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/NoteSets/StatisticalClassifications/Queries/GetStatisticalClassifications/StatisticalClassificationDto.cs
@@ -16,11 +16,11 @@ namespace Presentation.Application.NoteSets.StatisticalClassifications.Queries.G
             //default english
             string language = "en";
             profile.CreateMap<NodeSet, StatisticalClassificationDto>()
-                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : null))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : null))
-                .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale.Text(language)))
-                .ForMember(d => d.Definition, opt => opt.MapFrom(s => s.Definition != null ? s.Definition.Text(language) : null))
-                .ForMember(d => d.Link, opt => opt.MapFrom(s => s.Link != null ? s.Link.Text(language) : null));
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : String.Empty))
+                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
+                .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty))
+                .ForMember(d => d.Definition, opt => opt.MapFrom(s => s.Definition != null ? s.Definition.Text(language) : String.Empty))
+                .ForMember(d => d.Link, opt => opt.MapFrom(s => s.Link != null ? s.Link.Text(language) : String.Empty));
         }
     }
 
