06d59b8da790c84803d92a4bcda8cefb771d3bcc
florianika <florianika@gmail.com>
2021-12-09 11:07:01 +0100

    test 3

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/RepresentedVariables/Queries/GetRepresentationDetails/ValueDomainDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/RepresentedVariables/Queries/GetRepresentationDetails/ValueDomainDto.cs
@@ -35,7 +35,7 @@ namespace Presentation.Application.RepresentedVariables.Queries.GetRepresentatio
                 .ForMember(d => d.DataType, opt => opt.MapFrom(s => s.DataType))
                 .ForMember(d => d.ValueSet, opt => {
                     opt.PreCondition(s => s.Type == ValueDomainType.ENUMERATED);
-                    opt.MapFrom(s => isLeveled && s.Scope.ToString() == ValueDomainScope.SUBSTANTIVE.ToString() ? s.Level.Nodes : s.NodeSet.Nodes);
+                    opt.MapFrom(s => isLeveled &&  (int)s.Scope == (int)ValueDomainScope.SUBSTANTIVE ? s.Level.Nodes : s.NodeSet.Nodes);
                     opt.NullSubstitute(new List<ValueItemDto>());
                 });
                 
