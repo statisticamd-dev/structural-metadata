5aa95b2b4166bc464dd8148ebeaa6162cf498cd0
florianika <florianika@gmail.com>
2021-12-09 19:08:46 +0100

    adding level name to statistical classification item

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/NoteSets/StatisticalClassifications/Queries/GetStatisticalClassificationDetails/StatisticalClassificationItemDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/NoteSets/StatisticalClassifications/Queries/GetStatisticalClassificationDetails/StatisticalClassificationItemDto.cs
@@ -12,7 +12,8 @@ namespace Presentation.Application.NoteSets.StatisticalClassifications.Queries.G
         public string Code { get; set; }
         public string Value { get; set; }
         public string Description { get; set; }
-        public int LevelNumber { get; set; }
+        public int? LevelNumber { get; set; }
+        public string LevelName { get; set; }
         public List<StatisticalClassificationItemDto> Children { get; set; }
 
         public void Mapping(Profile profile)
@@ -25,7 +26,8 @@ namespace Presentation.Application.NoteSets.StatisticalClassifications.Queries.G
                 .ForMember(d => d.Code, opt => opt.MapFrom(s => s.Code))
                 .ForMember(d => d.Value, opt => opt.MapFrom(s => s.Label != null ? s.Label.Value.Text(language) : String.Empty))
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
-                .ForMember(d => d.LevelNumber, opt => opt.MapFrom(s => s.Level.LevelNumber))
+                .ForMember(d => d.LevelNumber, opt => opt.MapFrom(s => s.Level != null ? s.Level.LevelNumber : (int?) null))
+                .ForMember(d => d.LevelName, opt => opt.MapFrom(s => s.Level != null && s.Level.Name != null ? s.Level.Name.Text(language) : String.Empty))
                 .ForMember(d => d.Children, opt => opt.MapFrom(s => s.Children));
         }
     }
