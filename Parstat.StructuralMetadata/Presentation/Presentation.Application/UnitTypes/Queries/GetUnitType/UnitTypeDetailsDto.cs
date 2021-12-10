29f30e26c412680a896b38a5a5e4ca2b51fb0d22
florianika <florianika@gmail.com>
2021-12-10 12:47:43 +0100

    fixing unit type dto

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/UnitTypes/Queries/GetUnitType/UnitTypeDetailsDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/UnitTypes/Queries/GetUnitType/UnitTypeDetailsDto.cs
@@ -17,8 +17,9 @@ namespace Presentation.Application.UnitTypes.Queries.GetUnitType
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : String.Empty))
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
                 .ForMember(d => d.Definition, opt => opt.MapFrom(s => s.Definition != null ? s.Definition.Text(language) : String.Empty))
-                .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty)
-                );
+                .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty))
+                .ForMember(d => d.Definition, opt => opt.MapFrom(s => s.Definition != null ? s.Definition.Text(language) : String.Empty))
+                .ForMember(d => d.Link, opt => opt.MapFrom(s => s.Link != null ? s.Link.Text(language) : String.Empty));
         }
     }
 }
