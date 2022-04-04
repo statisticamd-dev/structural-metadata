f8bbc4cb7278f47a305f707a3ac18185317cf05b
Florian Nika <florianika@gmail.com>
2022-04-04 14:44:11 +0200

    small fix

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/NodeSets/CodeLists/Queries/GetCodeLists/GetCodeListsQuery.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/NodeSets/CodeLists/Queries/GetCodeLists/GetCodeListsQuery.cs
@@ -28,7 +28,7 @@ namespace Presentation.Application.NodeSets.CodeLists.Queries.GetCodeLists
             public async Task<CodeListsVm> Handle(GetCodeListsQuery request, CancellationToken cancellationToken)
             {
                 var codeLists = await _context.NodeSets
-                    .Where(ns => ns.NodeSetType == NodeSetType.CODE_LIST)
+                    .Where(ns => ns.NodeSetType == NodeSetType.CODE_LIST || ns.NodeSetType == NodeSetType.SENTINEL_CODE_LIST)
                     .AsNoTracking()
                     .ProjectTo<CodeListDto>(_mapper.ConfigurationProvider, new Dictionary<string, object> {["language"] = request.Language})
                     .OrderBy(cl => cl.LocalId)
