9aec39d615dd10299cb38c6ca2b5969f2b91e234
Florian Nika <florianika@gmail.com>
2022-04-15 15:51:51 +0200

    adding another property to component

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSet/ComponentMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSet/ComponentMiniDto.cs
@@ -11,6 +11,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSet
     {
 
         public ComponentType Type { get; set; }
+        public long RepresentationId { get; set; }
         public string RepresentationLink { get; set; }
 
         public void Mapping(Profile profile)
@@ -22,6 +23,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSet
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name == null ? String.Empty : s.Name.Text(language)))
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
                 .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty))
+                .ForMember(d => d.RepresentationId, opt => opt.MapFrom(s => s.RepresentedVariableId))
                 .ForMember(d => d.RepresentationLink, opt => opt.MapFrom(s => "/metadata/structural/variables/representations/view/" + s.RepresentedVariable.Id));
         }
     }
