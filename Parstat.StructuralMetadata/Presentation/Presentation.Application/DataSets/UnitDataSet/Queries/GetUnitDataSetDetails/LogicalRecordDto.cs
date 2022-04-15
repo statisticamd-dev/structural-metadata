3c62815b01d2eb27d549efd99f1904d8594fff0b
Florian Nika <florianika@gmail.com>
2022-04-15 10:48:35 +0200

    test 7

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/LogicalRecordDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/LogicalRecordDto.cs
@@ -9,7 +9,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDe
 {
     public class LogicalRecordDto : AbstractIdentifiableArtefactDto, IMapFrom<LogicalRecord>
     {
-        //public List<ComponentDto> Components { get; set; }
+        public List<ComponentDto> Components { get; set; }
 
         public void Mapping(Profile profile)
         {
@@ -20,7 +20,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDe
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name == null ? String.Empty : s.Name.Text(language)))
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
                 .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty))
-                /* .ForMember(d => d.Components, opt => opt.MapFrom(s => s.Components)) */;
+                .ForMember(d => d.Components, opt => opt.MapFrom(s => s.Components));
         }
     }
 }
