b876c0898af9f9fa0bae77c4b7840e85719a7508
Florian Nika <florianika@gmail.com>
2022-04-15 09:59:33 +0200

    testing get dataset qery

---
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/SubstantiveValueDomainMiniDto.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Presentation.Application.Common.Mappings;
+using Presentation.Application.Common.Models.StructuralMetadata.Abstracts;
+using Presentation.Common.Domain.StructuralMetadata.Enums;
+using Presentation.Domain.StructuralMetadata.Entities.Gsim.Concept;
+
+namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDetails
+{
+    public class SubstantiveValueDomainMiniDto : AbstractBaseDto, IMapFrom<ValueDomain>
+    {
+        public string Name { get; set; }
+        public ValueDomainType Type { get; set; }
+        public string Expression { get; set; }
+        public DataType DataType { get; set; }
+        public string MeasurementUnit { get; set; }
+        //public LevelDto NoteSetLevel { get; set; }
+        public List<ValueItemMiniDto> ValueSet { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            //language parameter from request
+            //default english
+            string language = "en";
+            profile.CreateMap<ValueDomain, SubstantiveValueDomainMiniDto>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, opt => opt.MapFrom(s=> s.Name.Text(language)))
+                .ForMember(d => d.MeasurementUnit, opt => opt.MapFrom(s => s.MeasurementUnit != null ? s.MeasurementUnit.Abbreviation : String.Empty))
+                .ForMember(d => d.Type, opt => opt.MapFrom(s => s.Type))
+                .ForMember(d => d.Expression, opt => opt.MapFrom(s => s.Expression))
+                .ForMember(d => d.DataType, opt => opt.MapFrom(s => s.DataType))
+               /*  .ForMember(d => d.ValueSet, opt => {
+                    opt.PreCondition(s => s.Type == ValueDomainType.ENUMERATED);
+                    opt.MapFrom(s => s.LevelId.HasValue ? s.Level.Nodes.OrderBy(n => n.Code) : s.NodeSet.Nodes.OrderBy(n => n.Code));
+                    opt.NullSubstitute(new List<ValueItemMiniDto>());
+                }) */;
+                
+                //.ForMember(d => d.NoteSetLevel, opt => opt.MapFrom(s => s.Level != null ? s.Level : null));
+        } 
+    }
+}
