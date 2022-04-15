6462d02d9d5bd87c401dc17c064bf02ca57c02d9
Florian Nika <florianika@gmail.com>
2022-04-15 11:49:37 +0200

    antother test

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/SubstantiveValueDomainMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/SubstantiveValueDomainMiniDto.cs
@@ -12,6 +12,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDe
     public class SubstantiveValueDomainMiniDto : AbstractBaseDto, IMapFrom<ValueDomain>
     {
         public string Name { get; set; }
+        public bool IsLeveled { get; set; } = false;
         public ValueDomainType Type { get; set; }
         public string Expression { get; set; }
         public DataType DataType { get; set; }
@@ -30,9 +31,10 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDe
                 .ForMember(d => d.Type, opt => opt.MapFrom(s => s.Type))
                 .ForMember(d => d.Expression, opt => opt.MapFrom(s => s.Expression))
                 .ForMember(d => d.DataType, opt => opt.MapFrom(s => s.DataType))
+                .ForMember(d => d.IsLeveled, opt => opt.MapFrom(s => s.LevelId.HasValue))
                 .ForMember(d => d.ValueSet, opt => {
                     opt.PreCondition(s => s.Type == ValueDomainType.ENUMERATED);
-                    opt.MapFrom(s => s.Level != null ? s.Level.Nodes.OrderBy(n => n.Code) : s.NodeSet.Nodes.OrderBy(n => n.Code));
+                    opt.MapFrom((s,d) => d.IsLeveled ? s.Level.Nodes.OrderBy(n => n.Code) : s.NodeSet.Nodes.OrderBy(n => n.Code));
                     opt.NullSubstitute(new List<ValueItemMiniDto>());
                 });
                 
