a5e6b9e648cdcc18c987060ab43ffff0a7af98d3
Florian Nika <florianika@gmail.com>
2022-04-15 13:45:31 +0200

    another fix

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/RepresentationMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSetDetails/RepresentationMiniDto.cs
@@ -9,6 +9,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDe
     public class RepresentationMiniDto : AbstractBaseDto, IMapFrom<RepresentedVariable>
     {
         public string Variable { get; set; }
+        public string Link { get; set; }
         public SentinelValueDomainMiniDto SentinelValueDomain { get; set; }
         public SubstantiveValueDomainMiniDto SubstantiveValueDomain { get; set; }
 
@@ -19,6 +20,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSetDe
             string language = "en";
             profile.CreateMap<RepresentedVariable, RepresentationMiniDto>()
                 .ForMember(d => d.Variable, opt => opt.MapFrom(s => s.Variable != null && s.Variable.Name != null ? s.Variable.Name.Text(language) : String.Empty))
+                .ForMember(d => d.Link, opt => opt.MapFrom(s => "/metadata/structural/variables/representations/view/" + s.Id))
                 .ForMember(d => d.SubstantiveValueDomain, opt => opt.MapFrom(s => s.SubstantiveValueDomain != null ? s.SubstantiveValueDomain : null))
                 .ForMember(d => d.SentinelValueDomain, opt => opt.MapFrom(s => s.SentinelValueDomain != null ? s.SentinelValueDomain : null)) ;
         }
