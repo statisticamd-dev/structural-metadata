dda8fe003e30ef416fbbe644513fd60c99c48b38
Enooo <enian.qosja@gmail.com>
2022-04-16 21:21:28 +0200

    Modified GetMeasurementUnits query to support search #42

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/MeasurementUnits/Queries/GetMeasurementUnits/GetMeasurementUnitsQuery.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/MeasurementUnits/Queries/GetMeasurementUnits/GetMeasurementUnitsQuery.cs
@@ -8,17 +8,20 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Presentation.Application.Common.Interfaces;
 using Presentation.Application.Common.Requests;
+using Presentation.Domain.StructuralMetadata.Entities.Gsim.Concept;
 
 namespace Presentation.Application.MeasurementUnits.Queries.GetMeasurementUnits
 {
     public class GetMeasurementUnitsQuery : AbstractRequest, IRequest<MeasurementUnitsVm>
     {
+        public string Name { get; set; }
+
         public class GetMeasurementUnitsQueryHandler : IRequestHandler<GetMeasurementUnitsQuery, MeasurementUnitsVm>
         {
             private readonly IStructuralMetadataDbContext _context;
             private readonly IMapper _mapper;
 
-            public GetMeasurementUnitsQueryHandler(IStructuralMetadataDbContext context, IMapper mapper) 
+            public GetMeasurementUnitsQueryHandler(IStructuralMetadataDbContext context, IMapper mapper)
             {
                 _context = context;
                 _mapper = mapper;
@@ -26,9 +29,10 @@ namespace Presentation.Application.MeasurementUnits.Queries.GetMeasurementUnits
 
             public async Task<MeasurementUnitsVm> Handle(GetMeasurementUnitsQuery request, CancellationToken cancellationToken)
             {
-                var measurementUnits = await _context.MeasurementUnits
+                var measurementUnitsQuery = CreateQuery(request.Name, request.Language);
+                var measurementUnits = await measurementUnitsQuery
                     .AsNoTracking()
-                    .ProjectTo<MeasurementUnitDto>(_mapper.ConfigurationProvider, new Dictionary<string, object> {["language"] = request.Language})
+                    .ProjectTo<MeasurementUnitDto>(_mapper.ConfigurationProvider, new Dictionary<string, object> { ["language"] = request.Language })
                     .OrderBy(mu => mu.LocalId)
                     .ToListAsync(cancellationToken);
 
@@ -38,6 +42,19 @@ namespace Presentation.Application.MeasurementUnits.Queries.GetMeasurementUnits
                 };
 
                 return vm;
+            }
+
+            private IQueryable<MeasurementUnit> CreateQuery(string name, string language)
+            {
+                if (string.IsNullOrWhiteSpace(name)) return _context.MeasurementUnits;
+
+                return language switch
+                {
+                    "en" => _context.MeasurementUnits.Where(ns => EF.Functions.ILike(ns.Name.En.ToUpper(), $"%{name.ToUpper()}%") || EF.Functions.ILike(ns.LocalId.ToUpper(), $"%{name.ToUpper()}%")),
+                    "ru" => _context.MeasurementUnits.Where(ns => EF.Functions.ILike(ns.Name.Ru.ToUpper(), $"%{name.ToUpper()}%") || EF.Functions.ILike(ns.LocalId.ToUpper(), $"%{name.ToUpper()}%")),
+                    "ro" => _context.MeasurementUnits.Where(ns => EF.Functions.ILike(ns.Name.Ro.ToUpper(), $"%{name.ToUpper()}%") || EF.Functions.ILike(ns.LocalId.ToUpper(), $"%{name.ToUpper()}%")),
+                    _ => _context.MeasurementUnits,
+                };
             }
         }
     }
--- Parstat.StructuralMetadata/Presentation/Presentation.WebApi/Controllers/MeasurementUnit/OpenMeasurementUnitController.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.WebApi/Controllers/MeasurementUnit/OpenMeasurementUnitController.cs
@@ -1,7 +1,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Presentation.Application.MeasurementUnits.Commands.CreateMeasurementUnit;
 using Presentation.Application.MeasurementUnits.Queries.GetMeasurementUnit;
 using Presentation.Application.MeasurementUnits.Queries.GetMeasurementUnits;
 
@@ -11,11 +10,14 @@ namespace Presentation.WebApi.Controllers
     {
         [HttpGet]
         [ProducesResponseType(typeof(MeasurementUnitsVm), StatusCodes.Status200OK)]
-        public async Task<ActionResult<MeasurementUnitsVm>> GetAll(string language) => Ok(await Mediator.Send(new GetMeasurementUnitsQuery {Language = language}));
+        public async Task<ActionResult<MeasurementUnitsVm>> GetAll(string language) => Ok(await Mediator.Send(new GetMeasurementUnitsQuery { Language = language }));
 
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(MeasurementUnitVm), StatusCodes.Status200OK)]
-        public async Task<ActionResult<MeasurementUnitVm>> Get(long id, string language) => Ok(await Mediator.Send(new GetMeasurementUnitQuery {Id = id, Language = language}));
+        public async Task<ActionResult<MeasurementUnitVm>> Get(long id, string language) => Ok(await Mediator.Send(new GetMeasurementUnitQuery { Id = id, Language = language }));
 
+        [HttpGet("{name}")]
+        [ProducesResponseType(typeof(MeasurementUnitsVm), StatusCodes.Status200OK)]
+        public async Task<ActionResult<MeasurementUnitsVm>> GetByName(string name, string language) => Ok(await Mediator.Send(new GetMeasurementUnitsQuery { Name = name, Language = language }));
     }
 }
