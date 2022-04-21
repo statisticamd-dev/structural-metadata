ccb820f1dd614ad954c7680da37dd6feee4c057a
Florian Nika <florianika@gmail.com>
2022-04-21 13:30:44 +0200

    a minor fix

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSet/LogicalRecordMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSet/LogicalRecordMiniDto.cs
@@ -24,7 +24,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSet
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
                 .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty))
                 .ForMember(d => d.UnitType, opt => opt.MapFrom(s => s.UnitType))
-                .ForMember(d => d.ParentRecord, opt => opt.MapFrom(s => s.Parent.Name))
+                .ForMember(d => d.ParentRecord, opt => opt.MapFrom(s => s.Parent != null && s.Parent.Name != null ? s.Parent.Name.Text(language) : String.Empty))
                 .ForMember(d => d.Components, opt => {
                     opt.PreCondition(s => s.Components.Count > 0);
                     opt.MapFrom(s => s.Components);
