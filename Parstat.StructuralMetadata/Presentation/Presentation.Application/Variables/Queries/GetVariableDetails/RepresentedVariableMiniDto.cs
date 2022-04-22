93ba6113f4d520aa4c5a07c1118126fac34c353b
Florian Nika <florianika@gmail.com>
2022-04-22 14:15:59 +0200

    adding sentinel and substantive vd in representation

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/Variables/Queries/GetVariableDetails/RepresentedVariableMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/Variables/Queries/GetVariableDetails/RepresentedVariableMiniDto.cs
@@ -12,6 +12,8 @@ namespace Presentation.Application.Variables.Queries.GetVariableDetails
         public string LocalId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public SubstantiveValueDomainMiniDto SubstantiveValueDomain { get; set; }
+        public SentinelValueDomainMiniDto SentinelValueDomain { get; set; }
 
         public void Mapping(Profile profile)
         {
@@ -22,7 +24,9 @@ namespace Presentation.Application.Variables.Queries.GetVariableDetails
                 .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
                 .ForMember(d => d.LocalId, opt => opt.MapFrom(s => s.LocalId))
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : String.Empty))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty));
+                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
+                .ForMember(d => d.SubstantiveValueDomain, opt => opt.MapFrom(s => s.SubstantiveValueDomain))
+                .ForMember(d => d.SentinelValueDomain, opt => opt.MapFrom(s => s.SentinelValueDomain != null ? s.SentinelValueDomain : null));
         }
     }
 }
---
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/Variables/Queries/GetVariableDetails/SentinelValueDomainMiniDto.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Presentation.Application.Common.Mappings;
+using Presentation.Application.Common.Models.StructuralMetadata.Abstracts;
+using Presentation.Common.Domain.StructuralMetadata.Enums;
+using Presentation.Domain.StructuralMetadata.Entities.Gsim.Concept;
+
+namespace Presentation.Application.Variables.Queries.GetVariableDetails
+{
+    public class SentinelValueDomainMiniDto : AbstractBaseDto, IMapFrom<ValueDomain>
+    {
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            //language parameter from request
+            //default english
+            string language = "en";
+            profile.CreateMap<ValueDomain, SentinelValueDomainMiniDto>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : String.Empty))
+                ;
+                
+                //.ForMember(d => d.NoteSetLevel, opt => opt.MapFrom(s => s.Level != null ? s.Level : null));
+        } 
+    }
+}
