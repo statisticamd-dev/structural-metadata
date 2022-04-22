43299fa1903da9514180e825f8af43f6584733ac
Florian Nika <florianika@gmail.com>
2022-04-22 09:13:33 +0200

    testing

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSet/LogicalRecordMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/UnitDataSet/Queries/GetUnitDataSet/LogicalRecordMiniDto.cs
@@ -10,7 +10,8 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSet
 {
     public class LogicalRecordMiniDto : AbstractIdentifiableArtefactDto, IMapFrom<LogicalRecord>
     {
-        public String UnitType { get; set; }
+        public string UnitType { get; set; }
+        public string ParentRecord { get; set; }
         public List<ComponentMiniDto> Components { get; set; }
 
         public void Mapping(Profile profile)
@@ -23,7 +24,7 @@ namespace Presentation.Application.DataSets.UnitDataSet.Queries.GetUnitDataSet
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
                 .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty))
                 .ForMember(d => d.UnitType, opt => opt.MapFrom(s => s.UnitType != null && s.UnitType.Name != null ? s.UnitType.Name.Text(language) : String.Empty))
-                //.ForMember(d => d.ParentRecord, opt => opt.MapFrom(s => s.Parent != null && s.Parent.Name != null ? s.Parent.Name.Text(language) : String.Empty))
+                .ForMember(d => d.ParentRecord, opt => opt.MapFrom(s => s.Parent != null ? s.Parent.LocalId : String.Empty))
                 .ForMember(d => d.Components, opt => {
                     opt.PreCondition(s => s.Components.Count > 0);
                     opt.MapFrom(s => s.Components);
