ed0730f08b0532f1f9c2f8b7e0f95f52581b6bf0
Enooo <enian.qosja@gmail.com>
2022-04-23 20:01:15 +0200

    Created api method to create new DataStructure #46

---
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataSets/DataStructures/Commands/CreateDataStructureCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Presentation.Application.DataSets.DataStructures.Commands
+{
+    public class CreateDataStructureCommandValidator : AbstractValidator<CreateDataStructureCommand>
+    {
+        public CreateDataStructureCommandValidator()
+        {
+            RuleFor(x => x.LocalId).MinimumLength(1).NotEmpty().NotNull();
+            RuleFor(x => x.Name).MinimumLength(3).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ Parstat.StructuralMetadata/Presentation/Presentation.WebApi/Controllers/DataStructure/ClosedDataStructureController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Application.DataSets.DataStructures.Commands;
+using System.Threading.Tasks;
+
+namespace Presentation.WebApi.Controllers
+{
+    public class ClosedDataStructureController : BaseController
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Create([FromBody] CreateDataStructureCommand command, string language)
+        {
+            command.Language = language;
+            return Ok(await Mediator.Send(command));
+        }
+      
+    }
+}
