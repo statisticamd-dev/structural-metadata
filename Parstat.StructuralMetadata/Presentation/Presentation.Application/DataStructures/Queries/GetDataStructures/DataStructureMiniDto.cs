cc99b560b82fd7b757e8373f223aa4ec8c38565e
Florian Nika <florianika@gmail.com>
2022-04-26 16:17:01 +0200

    fixing swagger

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataStructures/Queries/GetDataStructures/DataStructureMiniDto.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataStructures/Queries/GetDataStructures/DataStructureMiniDto.cs
@@ -6,7 +6,7 @@ using Presentation.Domain.StructuralMetadata.Entities.Gsim.Structure;
 
 namespace Presentation.Application.DataStructures.Queries.GetDataStructures
 {
-    public class DataStructureDto : AbstractIdentifiableArtefactDto, IMapFrom<DataStructure>
+    public class DataStructureMiniDto : AbstractIdentifiableArtefactDto, IMapFrom<DataStructure>
     {
         public string Group { get; set; }
 
@@ -15,7 +15,7 @@ namespace Presentation.Application.DataStructures.Queries.GetDataStructures
             //language parameter from  request
             //default english
             string language = "en";
-            profile.CreateMap<DataStructure, DataStructureDto>()
+            profile.CreateMap<DataStructure, DataStructureMiniDto>()
                 .ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name != null ? s.Name.Text(language) : String.Empty))
                 .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description != null ? s.Description.Text(language) : String.Empty))
                 .ForMember(d => d.VersionRationale, opt => opt.MapFrom(s => s.VersionRationale != null ? s.VersionRationale.Text(language) : String.Empty)
