a3c2e917b5550f0ce6f9e8d8f37021d14d8a6139
Florian Nika <florianika@gmail.com>
2022-04-29 15:56:07 +0200

    #52 code review

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataStructures/Commands/AddComponent/AddComponentCommand.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataStructures/Commands/AddComponent/AddComponentCommand.cs
@@ -3,11 +3,13 @@ using Presentation.Application.Common.Interfaces;
 using Presentation.Application.Common.Requests;
 using Presentation.Common.Domain.StructuralMetadata.Enums;
 using Presentation.Domain;
+using Presentation.Application.Common.Exceptions;
 using Presentation.Domain.StructuralMetadata.Entities.Gsim.Structure;
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Presentation.Application.DataStructures.Commands.AddComponent
 {
@@ -17,7 +19,7 @@ namespace Presentation.Application.DataStructures.Commands.AddComponent
         public string Name { get; set; }
         public string Description { get; set; }
         public long DataStructureId { get; set; }
-        public List<LogicalRecord> Records { get; set; }
+        public List<long> Records { get; set; }
 
         public class Handler : IRequestHandler<AddComponentCommand, long>
         {
@@ -30,14 +32,21 @@ namespace Presentation.Application.DataStructures.Commands.AddComponent
             public async Task<long> Handle(AddComponentCommand request, CancellationToken cancellationToken)
             {
                 Enum.TryParse(request.Language, true, out Language language);
-
+                var dataStructure = await _context.DataStructures.FindAsync(request.DataStructureId, cancellationToken);
+                if(dataStructure == null)
+                {
+                    throw new NotFoundException(nameof(DataStructure), request.DataStructureId);
+                }
+                var records = dataStructure.LogicalRecords
+                        .Where(lr =>request.Records.Contains(lr.Id) ).ToList();
+                        
                 var component = new Component 
                 {
                     LocalId = request.LocalId,
                     Name = MultilanguageString.Init(language, request.Name),
                     Description = MultilanguageString.Init(language, request.Description),
                     DataStructureId = request.DataStructureId,
-                    Records = request.Records
+                    Records = records,
                 };
 
                   _context.Components.Add(component);
