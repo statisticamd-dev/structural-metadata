a804c6beae38dc3ea03c90c05a2dea5dbde5300a
Enooo <enian.qosja@gmail.com>
2022-05-16 20:53:31 +0200

    Fix UpdateComponent

--- Parstat.StructuralMetadata/Presentation/Presentation.Application/DataStructures/UnitDataStructure/Commands/UpdateRecord/UpdateRecordCommand.cs
+++ Parstat.StructuralMetadata/Presentation/Presentation.Application/DataStructures/UnitDataStructure/Commands/UpdateRecord/UpdateRecordCommand.cs
@@ -4,10 +4,8 @@ using Presentation.Application.Common.Exceptions;
 using Presentation.Application.Common.Interfaces;
 using Presentation.Application.Common.Requests;
 using Presentation.Common.Domain.StructuralMetadata.Enums;
-using Presentation.Domain.StructuralMetadata.Entities.Gsim.Concept;
 using Presentation.Domain.StructuralMetadata.Entities.Gsim.Structure;
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,7 +20,6 @@ namespace Presentation.Application.DataStructures.UnitDataStructure.Commands.Upd
         public DateTime? VersionDate { get; set; }
         public string VersionRationale { get; set; }
         public long DataStructureId { get; set; }
-        public long UnitTypeId { get; set; }
         public long? ParentId { get; set; }
 
         public class Handler : IRequestHandler<UpdateRecordCommand, Unit>
@@ -36,28 +33,29 @@ namespace Presentation.Application.DataStructures.UnitDataStructure.Commands.Upd
             public async Task<Unit> Handle(UpdateRecordCommand request, CancellationToken cancellationToken)
             {
                 Enum.TryParse(request.Language, true, out Language language);
-                
-                var dataStructure = await GetDataStructureAsync(request.DataStructureId);
-                var unitType = await GetUnitTypeAsync(request.UnitTypeId);
-                var record = dataStructure.LogicalRecords.Where(lr => lr.Id == request.RecordId).FirstOrDefault();
-                if (record == null)
+
+                var entity = await _context.LogicalRecords.SingleOrDefaultAsync(lr => lr.Id == request.RecordId && lr.DataStructureId == request.DataStructureId);
+
+                if (entity == null)
                 {
-                    throw new NotFoundException(nameof(LogicalRecord), request.RecordId );
+                    throw new NotFoundException(nameof(LogicalRecord), request.RecordId);
                 }
-                var parentRecord = dataStructure.LogicalRecords.Where(lr => lr.Id == request.ParentId).FirstOrDefault();
-                record.Name.AddText(language, request.Name);
-                record.Description.AddText(language, request.Description);
-                record.VersionRationale.AddText(language, request.VersionRationale);
+
+                entity.Name.AddText(language, request.Name);
+                entity.Description.AddText(language, request.Description);
+                entity.VersionRationale.AddText(language, request.VersionRationale);
+
                 if (!string.IsNullOrWhiteSpace(request.Version))
                 {
-                    record.Version = request.Version;
+                    entity.Version = request.Version;
                 }
+
                 if (request.VersionDate.HasValue)
                 {
-                    record.VersionDate = request.VersionDate.Value;
+                    entity.VersionDate = request.VersionDate.Value;
                 }
-                record.Parent = parentRecord;    
-                record.UnitType = unitType;            
+
+                entity.ParentId = request.ParentId;
 
                 await _context.SaveChangesAsync(cancellationToken);
 
@@ -65,35 +63,6 @@ namespace Presentation.Application.DataStructures.UnitDataStructure.Commands.Upd
 
                 return Unit.Value;
             }
-
-            private async Task<DataStructure> GetDataStructureAsync(long dataStructureId) 
-            {
-                var dataStructure = await _context.DataStructures
-                                                        .Where(ds => ds.Id == dataStructureId)
-                                                        .Include(ds => ds.LogicalRecords)
-                                                        .SingleOrDefaultAsync();
-
-                if(dataStructure == null) 
-                {
-                    throw new NotFoundException(nameof(DataStructure), dataStructureId );
-                }   
-                return dataStructure;
-
-            }
-
-            private async Task<UnitType> GetUnitTypeAsync(long untiTypeId) 
-            {
-                var unitType = await _context.UnitTypes
-                                                        .Where(ut => ut.Id == untiTypeId)
-                                                        .SingleOrDefaultAsync();
-
-                if(unitType == null) 
-                {
-                    throw new NotFoundException(nameof(UnitType), untiTypeId );
-                }   
-                return unitType;
-
-            }
-        }        
+        }
     }
 }
